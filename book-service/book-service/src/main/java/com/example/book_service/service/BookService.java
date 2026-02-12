f2d52160acc3fa9b6e170548f5158ffbe62867bb
Geetha-005 <chillageetha323@gmail.com>
2026-02-12 19:15:05 +0530

    adding cache to the book-service

--- book-service/book-service/src/main/java/com/example/book_service/service/BookService.java
+++ book-service/book-service/src/main/java/com/example/book_service/service/BookService.java
@@ -3,6 +3,8 @@ package com.example.book_service.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
 import com.example.book_service.model.Book;
@@ -17,7 +19,7 @@ public class BookService {
     public List<Book> getAllBooks() {
         return bookRepository.findAll();
     }
-
+     @Cacheable(value = "books", key = "#id")
     public Book getBookById(Long id) {
         return bookRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Book not found"));
@@ -26,7 +28,7 @@ public class BookService {
     public Book addBook(Book book) {
         return bookRepository.save(book);
     }
-
+    @CacheEvict(value = "books", key = "#book.id")
     public Book updateBook(Long id, Book book) {
         Book existing = getBookById(id);
         existing.setTitle(book.getTitle());
@@ -35,7 +37,8 @@ public class BookService {
         existing.setStock(book.getStock());
         return bookRepository.save(existing);
     }
-
+    
+    @CacheEvict(value = "books", key = "#id")
     public void deleteBook(Long id) {
         bookRepository.deleteById(id);
     }
